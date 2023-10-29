8c9dd456c70609966140714992e4bb9c2d7a1eea
PeterPauk <petkopauk@gmail.com>
2023-10-29 16:25:10 +0100

    light effects and flashlight

--- Utility/weapon.gd
+++ Utility/weapon.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var sprite = $Sprite2D
+@onready var flashlight = $Light/PointLight2D
 
 @export var rotation_speed = 25
 var angle
@@ -8,14 +9,19 @@ var angle
 func _physics_process(delta):
 	if angle:
 		global_rotation = lerp_angle(global_rotation, angle, delta * rotation_speed)
+	print(flashlight.position.y)
 
 func _input(event):
 	angle = (get_global_mouse_position()-global_position).angle()
-	#gun flipping based on mouse position
+	
 	if get_global_mouse_position().x > sprite.global_position.x:
-		sprite.flip_v = false;	
+		sprite.flip_v = false;
+		flashlight.position.y = 30
 	else:
 		sprite.flip_v = true;
+		flashlight.position.y = 24
+		
+		
 	
 		
 
